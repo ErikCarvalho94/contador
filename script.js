a8631a672877bfcce0bdee33e286407af39b6d70
Erik Carvalho <erik.carvalho94@hotmail.com>
2025-05-04 22:29:17 -0300

    adicionado botao color contrast

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const valorElemento = document.getElementById("valor");
 const incrementarBotao = document.getElementById("incrementar");
 const decrementarBotao = document.getElementById("decrementar");
 const zerarBotao = document.getElementById("zerar");
+const alterarCoresBotao = document.getElementById("alterarCores");
+const body = document.body;
+const container = document.getElementsByClassName("container")[0];
 
 let contador = 0;
 
@@ -22,6 +25,18 @@ decrementarBotao.addEventListener('click', function(){
 zerarBotao.addEventListener('click', function(){
   contador = 0;
   atualizarValor();
+})
+
+alterarCoresBotao.addEventListener('click', function() {
+  if(body.style.backgroundColor === 'antiquewhite' && container.style.backgroundColor === 'yellowgreen'){
+    body.style.backgroundColor = '#333';
+    container.style.backgroundColor = '#555';
+    container.style.color = 'white';
+  } else {
+    body.style.backgroundColor = 'antiquewhite';
+    container.style.backgroundColor = 'yellowgreen';
+    container.style.color = 'black';
+  }
 })
 
 atualizarValor();

94a9a8ad49c0077083de4c324f538f1b14eca3de
Erik Carvalho <erik.carvalho94@hotmail.com>
2025-05-05 21:36:28 -0300

    feat: Implementa modo escuro com troca de ícone e acessibilidade aprimorada.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const incrementarBotao = document.getElementById("incrementar");
 const decrementarBotao = document.getElementById("decrementar");
 const zerarBotao = document.getElementById("zerar");
 const alterarCoresBotao = document.getElementById("alterarCores");
+const iconeAlterarCores = document.getElementById("iconeAlterarCores");
 const body = document.body;
 const container = document.getElementsByClassName("container")[0];
 
@@ -28,15 +29,25 @@ zerarBotao.addEventListener('click', function(){
 })
 
 alterarCoresBotao.addEventListener('click', function() {
-  if(body.style.backgroundColor === 'antiquewhite' && container.style.backgroundColor === 'yellowgreen'){
+  body.classList.toggle('modo-escuro');
+
+  if(body.classList.contains('modo-escuro')) {
     body.style.backgroundColor = '#333';
     container.style.backgroundColor = '#555';
     container.style.color = 'white';
+    iconeAlterarCores.src = './images/Sun.png';
   } else {
     body.style.backgroundColor = 'antiquewhite';
     container.style.backgroundColor = 'yellowgreen';
-    container.style.color = 'black';
+    iconeAlterarCores.src = './images/Moon.png';
   }
 })
 
-atualizarValor();
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Tab' && !event.shiftKey && document.activeElement === zerarBotao) {
+    event.preventDefault();
+  alterarCoresBotao.focus();
+  }
+})
+  
+  atualizarValor();
